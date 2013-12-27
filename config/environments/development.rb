fc4a35882ae9a048e352276af40c1c8996415540
mitzy <tlamy23@hotmail.com>
2013-12-27 10:20:33 -0800

    delete qtip files and remove command development configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,4 @@ HumanResourcesManagement::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  #config.serve_static_assets = false
 end
