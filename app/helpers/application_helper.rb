a557166b02f35ed8e449c96f1170354000dbabf8
mitzy <tlamy23@hotmail.com>
2014-01-13 10:26:42 -0800

    fix links home page and modified view calendar to show enable and disable days

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,28 +23,82 @@ module ApplicationHelper
 		HomeH.new(name).day_controller
 	end
 
+	def day_action(name)
+		HomeH.new(name).day_action
+	end
 
 	def calendar_days
 		days = []
-		day = {"label"=> "Sun","wknd"=> true,"clas"=> "col-md-1 col-xs-1 thumbnail alert-warning","clas2"=> "col-md-1 col-xs-1 thumbnail alert-warning"}
+		day = {"label"=> "Sun","wknd"=> true,"clas"=> "col-md-1 col-xs-1 thumbnail alert-warning","clas2"=> "col-md-1 col-xs-1 thumbnail alert-warning", "id"=> 0}
 		days.push(day)
-		day = {"label"=> "Mon","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success"}
+		day = {"label"=> "Mon","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success", "id"=> 1}
 		days.push(day)
-		day = {"label"=> "Tue","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success"}
+		day = {"label"=> "Tue","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success", "id"=> 2}
 		days.push(day)
-		day = {"label"=> "Wed","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success"}
+		day = {"label"=> "Wed","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success", "id"=> 3}
 		days.push(day)
-		day = {"label"=> "Thu","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success"}
+		day = {"label"=> "Thu","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success", "id"=> 4}
 		days.push(day)
-		day = {"label"=> "Fri","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success"}
+		day = {"label"=> "Fri","wknd"=> false,"clas"=> "col-md-2 col-xs-2 thumbnail alert-info","clas2"=> "col-md-2 col-xs-2 thumbnail alert-success", "id"=> 5}
 		days.push(day)
-		day = {"label"=> "Sat","wknd"=> true,"clas"=> "col-md-1 col-xs-1 thumbnail alert-warning","clas2"=> "col-md-1 col-xs-1 thumbnail alert-warning"}
+		day = {"label"=> "Sat","wknd"=> true,"clas"=> "col-md-1 col-xs-1 thumbnail alert-warning","clas2"=> "col-md-1 col-xs-1 thumbnail alert-warning", "id"=> 6}
 		days.push(day)
 		return days
 	end
+	
 	def calendar_day(day)
-  		day=Date.today if day.nil?
+  	day=Date.today if day.nil?
 		day= day - day.day
 		day += 1
 	end
+
+  def calendar_ndays(day)
+    day=calendar_day(day)
+    u=calendar_unless_days(day)
+    g=calendar_grater_days(day)
+    g=g+u
+    sd=day-u
+    days = {"start_day" => sd,"ndays" => g}
+    return days
+  end
+
+	def calendar_unless_days(start_day)
+		days=calendar_days
+    n=0
+		days.each do|d|
+      if @dayn.strftime("%a") == d["label"]
+        n=d["id"]
+      end
+    end
+    return n
+	end
+
+	def calendar_grater_days(start_day)
+    days=calendar_days
+    month=start_day.month
+    n=0
+    while month == start_day.month
+      days.each do|d|
+        start_day += 1
+        n += 1
+      end
+    end
+    return n
+	end
+
+  def calendar_dayclass(month,month_compare,object)
+    if month != month_compare
+      if object["wknd"]==true
+        return object["clas2"]="col-md-1 col-xs-1 thumbnail alert-danger"
+      else
+        return object["clas2"]="col-md-2 col-xs-2 thumbnail alert-danger"
+      end
+    else
+      if object["wknd"]==true
+        return object["clas2"]="col-md-1 col-xs-1 thumbnail alert-warning"
+      else
+        return object["clas2"]="col-md-2 col-xs-2 btn-info thumbnail alert-success"
+      end
+    end
+  end
 end
