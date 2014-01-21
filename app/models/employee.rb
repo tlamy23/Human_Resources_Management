eea19cad4dd6cc27d8ec7a2526344849e4833fd6
mitzy <tlamy23@hotmail.com>
2014-01-21 12:14:21 -0800

    integrate teams and project with employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ActiveRecord::Base
 	validates :name,:first_lastname,presence:true
+	belongs_to :team
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ActiveRecord::Base
+	belongs_to :employee, :foreign_key => 'leader'
+	alias_attribute :leader, :employee
 end
