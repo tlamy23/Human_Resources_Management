e1dfe929b304cf2b817f7035a111446055c93fa3
mitzy <tlamy23@hotmail.com>
2014-01-22 10:28:57 -0800

    add validates in teams and projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,5 @@ class Project < ActiveRecord::Base
 	belongs_to :employee, :foreign_key => 'admin'
 	alias_attribute :admin, :employee
 	belongs_to :calendar_type
+	validate :name,:calendar_type_id,:admin, presence:true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
 	belongs_to :employee, :foreign_key => 'leader'
 	alias_attribute :leader, :employee
+	validates :name,:leader, presence:true
 end
